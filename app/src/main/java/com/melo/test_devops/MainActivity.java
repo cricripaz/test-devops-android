180a03d7d534b25d77a376d82f3a539e913822aa
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-06-26 22:50:37 -0400

    INSTRUMENTATION TEST - FINAL - 2

--- app/src/main/java/com/melo/test_devops/MainActivity.java
+++ app/src/main/java/com/melo/test_devops/MainActivity.java
@@ -2,14 +2,13 @@ package com.melo.test_devops;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.melo.test_devops.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        //FFF
+
+        System.out.println("hola ");
+
     }
 
 }
